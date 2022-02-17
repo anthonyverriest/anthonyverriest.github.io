37b5e3ba95a6f016cbfa69f5bfb26ce87c4ac4cc
anthony verriest <averriest.dev@gmail.com>
2022-02-17 23:52:15 +0100

    fix: changed dist to docs folder

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
 const testPage1 = () => import('../components/pages/TestPage.vue')
-const testPage2 = () => import('../components/pages/TestPAge2.vue')
+const testPage2 = () => import('../components/pages/TestPage2.vue')
 
 const routes = [
     { path: '/one', component: testPage1 },
